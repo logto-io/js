2f9e7031b920c715e17ac9bfb7ea84dd076af4e2
wangsijie <i@sijie.wang>
2021-07-01 15:21:00 +0800

    koa

---
+++ koa/__tests__/koa.spec.ts
@@ -0,0 +1,23 @@
+import Koa, { Context } from 'koa';
+import request from 'supertest';
+import logto from '../src';
+
+describe('go through', () => {
+    let app: Koa;
+    beforeAll(() => {
+        app = new Koa();
+        app.use(logto());
+        app.use((ctx: Context) => {
+            ctx.body = 'hello ' + ctx.user.id;
+        });
+    })
+    test('find user', (done) => {
+        request.agent(app).get('/').set('authorization', 'bearer test-user-token').expect('hello test-user', done);
+    });
+    test('empty token should get 401', (done) => {
+        request.agent(app).get('/').expect(401, done);
+    });
+    test('invalid token should get 401', (done) => {
+        request.agent(app).get('/').set('authorization', 'bearer test-user-token1').expect(401, done);
+    });
+});
