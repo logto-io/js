7a5c23ef3f690bdc6493e38c8f8ae8825668d2ba
wangsijie <i@sijie.wang>
2021-07-20 22:26:07 +0800

    chore: fix lint on spec

--- packages/middleware/__tests__/index.spec.ts
+++ packages/middleware/__tests__/index.spec.ts
@@ -1,29 +1,29 @@
-import { extractBearerToken } from "../src";
+import { extractBearerToken } from '../src';
 
 describe('extractBearerToken', () => {
-    test('bearer testtoken', () => {
-        const token = extractBearerToken('bearer testtoken');
-        expect(token).toEqual('testtoken');
-    });
-    test('Bearer testtoken', () => {
-        const token = extractBearerToken('Bearer testtoken');
-        expect(token).toEqual('testtoken');
-    });
-    test('bearertesttoken', () => {
-        const token = extractBearerToken('bearertesttoken');
-        expect(token).toBeNull();
-    });
-    test('testtoken', () => {
-        const token = extractBearerToken('testtoken');
-        expect(token).toBeNull();
-    });
-    test('empty string', () => {
-        const token = extractBearerToken('');
-        expect(token).toBeNull();
-    });
-    test('empty input', () => {
-        // @ts-ignore
-        const token = extractBearerToken();
-        expect(token).toBeNull();
-    });
+  test('bearer testtoken', () => {
+    const token = extractBearerToken('bearer testtoken');
+    expect(token).toEqual('testtoken');
+  });
+  test('Bearer testtoken', () => {
+    const token = extractBearerToken('Bearer testtoken');
+    expect(token).toEqual('testtoken');
+  });
+  test('bearertesttoken', () => {
+    const token = extractBearerToken('bearertesttoken');
+    expect(token).toBeNull();
+  });
+  test('testtoken', () => {
+    const token = extractBearerToken('testtoken');
+    expect(token).toBeNull();
+  });
+  test('empty string', () => {
+    const token = extractBearerToken('');
+    expect(token).toBeNull();
+  });
+  test('empty input', () => {
+    // @ts-ignore
+    const token = extractBearerToken();
+    expect(token).toBeNull();
+  });
 });
