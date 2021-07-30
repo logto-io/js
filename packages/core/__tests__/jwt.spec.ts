007ee2fa17004471091e7903dd3cecf9b9c6b82e
wangsijie <i@sijie.wang>
2021-07-30 15:55:15 +0800

    jwt decode

---
+++ packages/core/__tests__/jwt.spec.ts
@@ -0,0 +1,15 @@
+import { decode } from '../src/jwt';
+
+const jwt_expired =
+  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkNza2w2SDRGR3NpLXE0QkVPT1BQOWJlbHNoRGFHZjd3RXViVU5KQllwQmsifQ.eyJzdWIiOiJ3anNadVc4VWpQd2ciLCJhdF9oYXNoIjoiM0V5QmZYbzVaX1pQSnRYQjIybnJ2USIsImF1ZCI6ImZvbyIsImV4cCI6MTYyNzYyNjk4NiwiaWF0IjoxNjI3NjIzMzg2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjMwMDEvb2lkYyJ9.ReuVmyX5WrPhTGj5chK8muQZsbj5DRY1TTUulFImX0K2s7VBtSZf0vy9sNilJeG8urUv6YjQeeFArUAlg7UzIFwFGDDM8wX-6-sNyBl3JB8Uc88XG44xwEzh-kyBk095aiK39PxOG876RNEKWpjTg3fi4-SHTGXkFvEZHCCJ6Eq7v1lhBTIqW3yKF3Y0uYvNq6IFkb6imJtUoVoHPX9noLXSMRHh6HYi71jlanFFKCsJEjFfXDOmrfG9ULrzM00cB0WeOWh1ipyWYyxlh57EDmMCUIIT7onXwwgeIAS5eh1mfi9KLM7oRWfMNtxwJyRxXQ9XuWZk7KlT9aoSNfUVEKe00pL6KBQ0uevJRbTbuH0XhRmZF7Vt5v1BPusLjFRnYQ93V8hlgMx596jv2OXqMoWone0UqGlCJhXy9lZJF5L9EH2AdyAnDe0OQ3UvOjkxzDqBgKBhvdKKtkTU10ZR9GWHHSbY9DyccG7HLub8m3gULU8j-4p8LDCJhvLtv-8wF4tNgxC0wvwIWfJavTo3DcMoECAOyGluugaAgvDT5w04kr2MIinet1ana9YN8iWuKnG06srfNncyJzztLqLWOb1cMbbvodcehlCJZXlRVmAZ1BN4-sANu_jsRBRnF60mzxRk2lJajSy_YkBgt2YpiGj3UQfwC2ok4kkO72VRpbk';
+const jwt_lasts_for_ever =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjMwMDEvb2lkYyIsImlhdCI6MTYyNzYyOTM2OCwiZXhwIjo0MDg5MDc4OTY4LCJhdWQiOiJmb28iLCJzdWIiOiJ3anNadVc4VWpQd2ciLCJhdF9oYXNoIjoiM0V5QmZYbzVaX1pQSnRYQjIybnJ2USJ9.j4wF6DhmBJlEpRhYdvX36DVuzet2fxEcQypDcbauOmc';
+describe('jwt', () => {
+  test('id_token decoe', () => {
+    const idToken = decode(jwt_lasts_for_ever);
+    expect(idToken.aud).toEqual('foo');
+  });
+  test('id_token decode: expired', () => {
+    expect(() => decode(jwt_expired)).toThrow();
+  });
+});
--- packages/core/src/jwt.ts
+++ packages/core/src/jwt.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import * as jwt from 'jsonwebtoken';
+import { IDToken } from './types.d';
 
 export interface JWK {
   kty: string;
@@ -16,4 +18,57 @@ export const fetchJwks = async (url: string): Promise<JWK[]> => {
     console.error(error);
     throw new Error('Error occurred during jwks fetching');
   }
+};
+
+export interface JWTVerifyOptions {
+  idToken: string;
+  audience?: string | string[];
+  issuer?: string;
+  subject?: string;
+}
+
+export const decode = (token: string) => {
+  const { payload } = jwt.decode(token, { complete: true });
+  const { iss, sub, aud, exp, iat, at_hash } = payload;
+
+  // Check exp manually
+  const now = new Date(Date.now());
+  const expDate = new Date(0);
+  expDate.setUTCSeconds(exp);
+  if (now > expDate) {
+    throw new Error(
+      `Expiration Time (exp) claim error in the ID token; current time (${now.toISOString()}) is after expiration time (${expDate.toISOString()})`
+    );
+  }
+
+  if (!iss) {
+    throw new Error('Issuer (iss) claim must be a string present in the ID token');
+  }
+
+  if (!sub) {
+    throw new Error('Subject (sub) claim must be a string present in the ID token');
+  }
+
+  if (!aud) {
+    throw new Error(
+      'Audience (aud) claim must be a string or any array of strings present in the ID token'
+    );
+  }
+
+  if (typeof at_hash !== 'string') {
+    throw new TypeError(
+      'Access Token Hash (at_hash) claim must be a string present in the ID token'
+    );
+  }
+
+  const idToken: IDToken = {
+    iss,
+    sub,
+    aud,
+    exp,
+    iat,
+    at_hash,
+  };
+
+  return idToken;
 };
