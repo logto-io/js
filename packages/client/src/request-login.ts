76931562cad2a115f0dbc3912ea4923b170ccaff
wangsijie <i@sijie.wang>
2021-10-15 15:20:34 +0800

    feat: requestLogin

---
+++ packages/client/src/request-login.ts
@@ -0,0 +1,24 @@
+import qs from 'query-string';
+
+import { generateCodeChallenge, generateCodeVerifier } from './generators';
+
+export const getLoginUrlAndCodeVerifier = (
+  baseUrl: string,
+  clientId: string,
+  redirectUri: string
+): [string, string] => {
+  const codeVerifier = generateCodeVerifier();
+  const codeChallenge = generateCodeChallenge(codeVerifier);
+
+  const url = `${baseUrl}?${qs.stringify({
+    client_id: clientId,
+    scope: 'openid offline_access',
+    response_type: 'code',
+    redirect_uri: [redirectUri],
+    prompt: 'consent',
+    code_challenge: codeChallenge,
+    code_challenge_method: 'S256',
+  })}`;
+
+  return [url, codeVerifier];
+};
