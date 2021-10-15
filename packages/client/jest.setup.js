023e2603602f8ed42e546c812ef502923138af16
wangsijie <i@sijie.wang>
2021-10-15 12:38:22 +0800

    test: jsdom axios config

--- packages/client/jest.setup.js
+++ packages/client/jest.setup.js
@@ -1,9 +1,14 @@
+/* eslint-disable node/prefer-global/text-decoder */
 /* eslint-disable @silverhand/fp/no-mutation */
 /* eslint-disable node/prefer-global/text-encoder */
 /* eslint-disable unicorn/prefer-module */
-const { TextEncoder } = require('util');
+const { TextEncoder, TextDecoder } = require('util');
 
 const { Crypto } = require('@peculiar/webcrypto');
+const axios = require('axios');
 
 global.crypto = new Crypto();
 global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+axios.defaults.adapter = require('axios/lib/adapters/http');
