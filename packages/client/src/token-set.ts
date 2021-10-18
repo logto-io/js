2e98b056d92b93f3fdd3e3588c3f996e715a179d
Wang Sijie <wangsijie@silverhand.io>
2021-10-18 18:09:48 +0800

    feat: token-set (#48)
    * feat: token-set
    
    * refactor: nowRoundToSec

---
+++ packages/client/src/token-set.ts
@@ -0,0 +1,35 @@
+import { TokenSetParameters } from './grant-token';
+import { decodeToken, IDToken, nowRoundToSec } from './utils';
+
+export default class TokenSet {
+  public accessToken: string;
+  public idToken: string;
+  public refreshToken: string;
+  public expiresAt = 0;
+  constructor(tokenSet: TokenSetParameters) {
+    this.accessToken = tokenSet.access_token;
+    this.expiresIn = tokenSet.expires_in;
+    this.idToken = tokenSet.id_token;
+    this.refreshToken = tokenSet.refresh_token;
+  }
+
+  get expiresIn(): number {
+    return Math.max(this.expiresAt - nowRoundToSec(), 0);
+  }
+
+  set expiresIn(value: number) {
+    this.expiresAt = nowRoundToSec() + value;
+  }
+
+  public expired(): boolean {
+    return this.expiresIn === 0;
+  }
+
+  public claims(): IDToken {
+    if (!this.idToken) {
+      throw new TypeError('id_token not present in TokenSet');
+    }
+
+    return decodeToken(this.idToken);
+  }
+}
--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -52,3 +52,5 @@ export const decodeToken = (token: string): IDToken => {
     throw new Error('invalid token: JSON parse failed');
   }
 };
+
+export const nowRoundToSec = () => Math.floor(Date.now() / 1000);
