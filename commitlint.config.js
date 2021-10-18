e600ba538035a7eeee401c20fd0c24b0bfaa8b46
simeng_li <simeng@silverhand.io>
2021-10-18 09:59:57 +0800

    ci(js): remove api commitlint rule
    Remove api commitlint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,4 @@
-const { rules } = require('@commitlint/config-conventional');
-
 /** @type {import('@commitlint/types').UserConfig} **/
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', [...rules['type-enum'][2], 'api']],
-  },
 };
