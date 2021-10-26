52b47c7850a1e10e2ed93930c9f60d075275a2a3
Ice He <icehe@silverhand.io>
2021-10-26 15:06:01 +0800

    refactor: replace crypto.getRandomValues() with nanoid.random()

--- packages/client/src/generators.ts
+++ packages/client/src/generators.ts
@@ -1,28 +1,51 @@
-import { fromUint8Array } from 'js-base64';
+// Import { BinaryLike, createHash } from 'crypto';
+// import * as crypto from 'crypto';
 
-function generateRandomData(length: number) {
-  const array = new Uint8Array(length);
-  crypto.getRandomValues(array);
-  return Array.from(array);
-}
+import { fromUint8Array } from 'js-base64';
+import { customAlphabet } from 'nanoid';
+
+import { encodeBase64 } from './utils';
 
 function generateRandomString(length: number) {
-  const randomData = generateRandomData(length);
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  const charCodes = randomData.map((randomNumber) =>
-    alphabet.charCodeAt(randomNumber & alphabet.length)
-  );
-
-  return String.fromCharCode(...charCodes);
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+  return customAlphabet(alphabet, length)();
 }
 
-export const CODE_VERIFIER_LEN = 96;
-export const generateCodeVerifier = (): string => generateRandomString(CODE_VERIFIER_LEN);
+/**
+ * @link https://datatracker.ietf.org/doc/html/rfc7636#section-4.1
+ */
+export const CODE_VERIFIER_MIN_LEN = 43;
+export const CODE_VERIFIER_MAX_LEN = 128;
 
+/**
+ * Generates random bytes and encodes them in url safe base64.
+ * @param length Number indicating the number of bytes to generate.
+ */
+export const generateRandom = (length = CODE_VERIFIER_MAX_LEN) =>
+  encodeBase64(generateRandomString(length));
+
+/**
+ * Generates random bytes and encodes them in url safe base64.
+ */
+export const generateState = () => generateRandom();
+
+/**
+ * Generates random bytes and encodes them in url safe base64.
+ */
+export const generateNonce = () => generateRandom();
+
+/**
+ * Generates random bytes and encodes them in url safe base64.
+ */
+export const generateCodeVerifier = () => generateRandom();
+
+/**
+ * Calculates the S256 PKCE code challenge for an arbitrary code verifier.
+ * Encodes in url safe base64.
+ * @param codeVerifier Code verifier to calculate the S256 code challenge for
+ */
 export const generateCodeChallenge = async (codeVerifier: string): Promise<string> => {
   const encoded = new TextEncoder().encode(codeVerifier);
   const challenge = new Uint8Array(await crypto.subtle.digest('SHA-256', encoded));
-
   return fromUint8Array(challenge).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 };
