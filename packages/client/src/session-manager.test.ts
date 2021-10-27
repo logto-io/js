9aadf06a72309fef9abddfe5d410e8d78a3c7971
Wang Sijie <wangsijie@silverhand.io>
2021-10-27 16:53:36 +0800

    feat: transaction manager (#51)
    * feat: transaction manager
    
    * refactor: rename to SessionManager
    
    * chore: optional
    
    * chore: use full name for vars
    
    * refactor: use SESSION_EXPIRES_SECONDS
    
    * fix: remove property session to keep SSOT
    
    * test: remove constructer test

---
+++ packages/client/src/session-manager.test.ts
@@ -0,0 +1,32 @@
+import SessionManager from './session-manager';
+import { SessionStorage } from './storage';
+
+const SESSION_MANAGER_KEY = 'LOGTO_SESSION_MANAGER';
+
+const transaction = {
+  codeVerifier: 'codeVerifier',
+  redirectUri: 'redirectUri',
+};
+
+describe('SessionManager', () => {
+  test('create and get', () => {
+    const sessionStorage = new SessionStorage();
+    const sessionManager = new SessionManager(sessionStorage);
+    sessionManager.create(transaction);
+    expect(sessionManager.get()).toMatchObject(transaction);
+  });
+
+  test('`create` should save data in storage', () => {
+    const sessionStorage = new SessionStorage();
+    const sessionManager = new SessionManager(sessionStorage);
+    sessionManager.create(transaction);
+    expect(sessionStorage.getItem(SESSION_MANAGER_KEY)).toMatchObject(transaction);
+  });
+
+  test('restore data from storage', () => {
+    const sessionStorage = new SessionStorage();
+    sessionStorage.setItem(SESSION_MANAGER_KEY, transaction);
+    const sessionManager = new SessionManager(sessionStorage);
+    expect(sessionManager.get()).toMatchObject(transaction);
+  });
+});
---
+++ packages/client/src/session-manager.ts
@@ -0,0 +1,29 @@
+import { Optional } from '@silverhand/essentials';
+
+import { ClientStorage } from './storage';
+
+const SESSEION_MANAGER_KEY = 'LOGTO_SESSION_MANAGER';
+const SESSION_EXPIRES_SECONDS = 86_400;
+
+interface Session {
+  codeVerifier: string;
+  redirectUri: string;
+}
+
+export default class SessionManager {
+  constructor(private readonly storage: ClientStorage) {}
+
+  public create(session: Session) {
+    this.storage.setItem(SESSEION_MANAGER_KEY, session, {
+      secondsUntilExpire: SESSION_EXPIRES_SECONDS,
+    });
+  }
+
+  public get(): Optional<Session> {
+    return this.storage.getItem(SESSEION_MANAGER_KEY);
+  }
+
+  public remove() {
+    this.storage.removeItem(SESSEION_MANAGER_KEY);
+  }
+}
