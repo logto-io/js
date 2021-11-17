951d316fc6efae955b0e79a0a7cc8f80bfe91bce
simeng_li <simeng@silverhand.io>
2021-11-17 11:42:08 +0800

    feat(react-sdk): update react sdk
    export PrivateRouter & useLogto out
    update tsx build type to es5
    add prepublish script

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,5 @@
 export * from '@logto/client';
 export * from './context';
 export * from './provider';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as useLogto} from './use-logto';
