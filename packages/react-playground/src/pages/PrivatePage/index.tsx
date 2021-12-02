45155c6369b12b040fbb7d57abf736dd3ae159f5
Mingqing Ye <darcyye@silverhand.io>
2021-12-02 12:25:16 +0800

    feat(react-playground): make encrypted user login info in url invisible

--- packages/react-playground/src/pages/PrivatePage/index.tsx
+++ packages/react-playground/src/pages/PrivatePage/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import queryString from 'query-string';
+import React, { useEffect } from 'react';
 
 import styles from './index.module.scss';
 
 const PrivatePage = () => {
+  useEffect(() => {
+    if (window.location.href.includes('?')) {
+      const { url: baseUrl, query: queryObject } = queryString.parseUrl(window.location.href);
+      if (baseUrl && Object.keys(queryObject).length > 0) {
+        const { code, state, ...filteredEntries } = queryObject;
+        const newUrl = queryString.stringifyUrl({ url: baseUrl, query: filteredEntries });
+        history.replaceState(history.state, '', newUrl);
+      }
+    }
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>Private Page</div>
