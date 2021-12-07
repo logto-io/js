96b93dcc34a2f1d336ec3271b39224331c226a26
simeng-li <simeng@silverhand.io>
2021-12-07 16:16:07 +0800

    refactor(client): remove onAuthStateChange callback method
    remove onAuthStateChange callback method of js SDK as it is nolonger needed

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,4 +2,4 @@ export * from '@logto/client';
 export * from './context';
 export * from './provider';
 export { default as ProtectedRoute } from './ProtectedRoute';
-export { default as useLogto} from './use-logto';
+export { default as useLogto } from './use-logto';
