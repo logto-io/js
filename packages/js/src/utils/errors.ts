954dc6d9f046e752f635248bcd87e15cf02fb63e
IceHe.xyz <icehe@silverhand.io>
2022-01-21 15:34:59 +0800

    feat(js): verifyIdToken (#127)

---
+++ packages/js/src/utils/errors.ts
@@ -0,0 +1,31 @@
+/* Copied from react-i18next/ts4.1/index.d.ts */
+// Normalize single namespace
+type AppendKeys<K1, K2> = `${K1 & string}.${K2 & string}`;
+type AppendKeys2<K1, K2> = `${K1 & string}.${Exclude<K2, keyof any[]> & string}`;
+type Normalize2<T, K = keyof T> = K extends keyof T
+  ? T[K] extends Record<string, any>
+    ? T[K] extends readonly any[]
+      ? AppendKeys2<K, keyof T[K]> | AppendKeys2<K, Normalize2<T[K]>>
+      : AppendKeys<K, keyof T[K]> | AppendKeys<K, Normalize2<T[K]>>
+    : never
+  : never;
+type Normalize<T> = keyof T | Normalize2<T>;
+
+const logtoErrorCodes = Object.freeze({
+  idToken: {
+    verification: {
+      invalidIat: 'Invalid issued at time',
+    },
+  },
+});
+
+export type LogtoErrorCode = Normalize<typeof logtoErrorCodes>;
+
+export class LogtoError extends Error {
+  code: LogtoErrorCode;
+
+  constructor(code: LogtoErrorCode, message?: string) {
+    super(message);
+    this.code = code;
+  }
+}
--- packages/js/src/utils/id-token.test.ts
+++ packages/js/src/utils/id-token.test.ts
@@ -1,7 +1,215 @@
-import { SignJWT, generateKeyPair } from 'jose';
+import { KeyObject } from 'crypto';
+
+import { createRemoteJWKSet, generateKeyPair, SignJWT } from 'jose';
+import nock from 'nock';
 import { StructError } from 'superstruct';
 
-import { decodeIdToken } from './id-token';
+import { LogtoError } from './errors';
+import { decodeIdToken, verifyIdToken } from './id-token';
+
+const createDefaultJwks = () => createRemoteJWKSet(new URL('https://logto.dev/oidc/jwks'));
+
+describe('verifyIdToken', () => {
+  test('valid ID Token, signed by RS256 algorithm, should not throw', async () => {
+    const alg = 'RS256';
+    const { privateKey, publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check environment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('qux')
+      .setExpirationTime('2h')
+      .setIssuedAt()
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'qux', 'foo', jwks)).resolves.not.toThrow();
+  });
+
+  test('valid ID Token, signed by ES512 algorithm, should not throw', async () => {
+    const alg = 'ES512';
+    const { privateKey, publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check environment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('qux')
+      .setExpirationTime('2h')
+      .setIssuedAt()
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'qux', 'foo', jwks)).resolves.not.toThrow();
+  });
+
+  test('mismatched signature should throw', async () => {
+    const alg = 'RS256';
+    const { privateKey } = await generateKeyPair(alg);
+    const { publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check envirionment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('foz')
+      .setExpirationTime('2h')
+      .setIssuedAt()
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'foo', 'baz', jwks)).rejects.toThrowError(
+      'signature verification failed'
+    );
+  });
+
+  test('mismatched issuer should throw', async () => {
+    const alg = 'RS256';
+    const { privateKey, publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check environment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('qux')
+      .setExpirationTime('2h')
+      .setIssuedAt()
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'qux', 'xxx', jwks)).rejects.toThrowError(
+      'unexpected "iss" claim value'
+    );
+  });
+
+  test('mismatched audience should throw', async () => {
+    const alg = 'RS256';
+    const { privateKey, publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check environment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('qux')
+      .setExpirationTime('2h')
+      .setIssuedAt()
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'xxx', 'foo', jwks)).rejects.toThrowError(
+      'unexpected "aud" claim value'
+    );
+  });
+
+  test('expired ID Token should throw', async () => {
+    const alg = 'RS256';
+    const { privateKey, publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check environment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('qux')
+      .setExpirationTime(Date.now() / 1000 - 1)
+      .setIssuedAt()
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'qux', 'foo', jwks)).rejects.toThrowError(
+      '"exp" claim timestamp check failed'
+    );
+  });
+
+  test('issued at time, too far away from current time, should throw', async () => {
+    const alg = 'RS256';
+    const { privateKey, publicKey } = await generateKeyPair(alg);
+
+    if (!(publicKey instanceof KeyObject)) {
+      throw new TypeError('key is not instanceof KeyObject, check environment');
+    }
+
+    const key = publicKey.export({ format: 'jwk' });
+    nock('https://logto.dev', { allowUnmocked: true })
+      .get('/oidc/jwks')
+      .reply(200, { keys: [key] });
+
+    const idToken = await new SignJWT({})
+      .setProtectedHeader({ alg })
+      .setIssuer('foo')
+      .setSubject('bar')
+      .setAudience('qux')
+      .setExpirationTime('2h')
+      .setIssuedAt(Date.now() / 1000 - 180)
+      .sign(privateKey);
+
+    const jwks = createDefaultJwks();
+
+    await expect(verifyIdToken(idToken, 'qux', 'foo', jwks)).rejects.toThrowError(
+      new LogtoError('idToken.verification.invalidIat')
+    );
+  });
+});
 
 describe('decodeIdToken', () => {
   test('decoding valid JWT should return claims', async () => {
@@ -98,7 +306,7 @@ describe('decodeIdToken', () => {
     expect(() => decodeIdToken(jwt)).toThrowError(StructError);
   });
 
-  test('decoding valid JWT without issued time should throw StructError', async () => {
+  test('decoding valid JWT without issued at time should throw StructError', async () => {
     const { privateKey } = await generateKeyPair('RS256');
     const jwt = await new SignJWT({})
       .setProtectedHeader({ alg: 'RS256' })
