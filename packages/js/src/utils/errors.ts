e636db4dcf284f45f4491547ef1314934b8c062b
IceHe.xyz <icehe@silverhand.io>
2022-01-24 15:41:21 +0800

    refactor(js): replace Normalize with NormalizeKeyPaths from @silverhand/essentials

--- packages/js/src/utils/errors.ts
+++ packages/js/src/utils/errors.ts
@@ -1,15 +1,4 @@
-/* Copied from react-i18next/ts4.1/index.d.ts */
-// Normalize single namespace
-type AppendKeys<K1, K2> = `${K1 & string}.${K2 & string}`;
-type AppendKeys2<K1, K2> = `${K1 & string}.${Exclude<K2, keyof any[]> & string}`;
-type Normalize2<T, K = keyof T> = K extends keyof T
-  ? T[K] extends Record<string, any>
-    ? T[K] extends readonly any[]
-      ? AppendKeys2<K, keyof T[K]> | AppendKeys2<K, Normalize2<T[K]>>
-      : AppendKeys<K, keyof T[K]> | AppendKeys<K, Normalize2<T[K]>>
-    : never
-  : never;
-type Normalize<T> = keyof T | Normalize2<T>;
+import { NormalizeKeyPaths } from '@silverhand/essentials';
 
 const logtoErrorCodes = Object.freeze({
   idToken: {
@@ -18,7 +7,7 @@ const logtoErrorCodes = Object.freeze({
   },
 });
 
-export type LogtoErrorCode = Normalize<typeof logtoErrorCodes>;
+export type LogtoErrorCode = NormalizeKeyPaths<typeof logtoErrorCodes>;
 
 export class LogtoError extends Error {
   code: LogtoErrorCode;
