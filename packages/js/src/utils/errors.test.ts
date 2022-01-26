02ccb4775dea97a6242832c0dc080455b70cbca7
IceHe.xyz <icehe@silverhand.io>
2022-01-26 14:47:22 +0800

    refactor(js): refactor requester and LogtoError, add LogtoRequestError

--- packages/js/src/utils/errors.test.ts
+++ packages/js/src/utils/errors.test.ts
@@ -1,25 +1,33 @@
-import { LogtoError, LogtoErrorCode } from './errors';
+import { LogtoError, LogtoErrorCode, LogtoRequestError } from './errors';
 
 describe('LogtoError', () => {
   test('new LogtoError should contain correct properties', () => {
-    const errorCode: LogtoErrorCode = 'callback_uri_verification.missing_code';
+    const code: LogtoErrorCode = 'callback_uri_verification.missing_code';
     const error = 'error_value';
     const errorDescription = 'error_description_content';
-    const logtoError = new LogtoError(errorCode, error, errorDescription);
-    expect(logtoError).toHaveProperty('code', errorCode);
+    const logtoError = new LogtoError(code, { error, errorDescription });
+    expect(logtoError).toHaveProperty('code', code);
     expect(logtoError).toHaveProperty('message', 'Missing code');
-    expect(logtoError).toHaveProperty('error', error);
-    expect(logtoError).toHaveProperty('errorDescription', errorDescription);
+    expect(logtoError).toHaveProperty('data', { error, errorDescription });
   });
 
   test('new LogtoError with error code, which is not related to unique message, should contain message equaling to error code', () => {
-    const errorCode: LogtoErrorCode = 'callback_uri_verification';
+    const code: LogtoErrorCode = 'callback_uri_verification';
     const error = 'error_value';
     const errorDescription = 'error_description_content';
-    const logtoError = new LogtoError(errorCode, error, errorDescription);
-    expect(logtoError).toHaveProperty('code', errorCode);
-    expect(logtoError).toHaveProperty('message', errorCode);
-    expect(logtoError).toHaveProperty('error', error);
-    expect(logtoError).toHaveProperty('errorDescription', errorDescription);
+    const logtoError = new LogtoError(code, { error, errorDescription });
+    expect(logtoError).toHaveProperty('code', code);
+    expect(logtoError).toHaveProperty('message', code);
+    expect(logtoError).toHaveProperty('data', { error, errorDescription });
+  });
+});
+
+describe('LogtoRequestError', () => {
+  test('new LogtoRequestError should contain correct properties', () => {
+    const code = 'some code';
+    const message = 'some message';
+    const logtoRequestError = new LogtoRequestError(code, message);
+    expect(logtoRequestError).toHaveProperty('code', code);
+    expect(logtoRequestError).toHaveProperty('message', message);
   });
 });
