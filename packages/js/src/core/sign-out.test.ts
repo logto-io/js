c7b1889c1adecb75df63dfd181b243a2899dd8d7
Charles Zhao <charleszhao@silverhand.io>
2022-01-29 18:39:30 +0800

    refactor(js): core function generateSignOutUri

--- packages/js/src/core/sign-out.test.ts
+++ packages/js/src/core/sign-out.test.ts
@@ -1,22 +1,22 @@
 import { generateSignOutUri } from './sign-out';
 
 describe('Generate sign-out URI', () => {
-  const endPoint = 'http://logto.dev/oidc/session/end';
+  const endSessionEndpoint = 'http://logto.dev/oidc/session/end';
   const idToken = 'id_token';
-  const redirectUri = 'http://localhost:3000';
+  const postLogoutRedirectUri = 'http://localhost:3000';
 
   test('Generate with both id token and redirect URI', async () => {
-    const signOutUri = generateSignOutUri(endPoint, idToken, redirectUri);
+    const signOutUri = generateSignOutUri({ endSessionEndpoint, idToken, postLogoutRedirectUri });
 
-    expect(signOutUri).toContain(`${endPoint}?`);
+    expect(signOutUri).toContain(`${endSessionEndpoint}?`);
     expect(signOutUri).toContain(`id_token_hint=${idToken}`);
     expect(signOutUri).toContain('post_logout_redirect_uri=http%3A%2F%2Flocalhost%3A3000');
   });
 
   test('Generate without redirect URI', async () => {
-    const signOutUri = generateSignOutUri(endPoint, idToken);
+    const signOutUri = generateSignOutUri({ endSessionEndpoint, idToken });
 
-    expect(signOutUri).toContain(`${endPoint}?`);
+    expect(signOutUri).toContain(`${endSessionEndpoint}?`);
     expect(signOutUri).toContain(`id_token_hint=${idToken}`);
     expect(signOutUri).not.toContain('post_logout_redirect_uri');
   });
