f9c9307364cf1a65c0203f4bda75f2f6df96ff15
IceHe.xyz <icehe@silverhand.io>
2022-01-30 14:28:20 +0800

    feat(js): revoke

---
+++ packages/js/src/core/revoke.test.ts
@@ -0,0 +1,20 @@
+import { revoke } from './revoke';
+
+describe('revoke', () => {
+  test('requester should called with correct parameters', async () => {
+    const revocationEndpoint = 'https://logto.dev/oidc/token/revocation';
+    const clientId = 'clientId';
+    const token = 'token';
+    const mockedRequester = jest.fn();
+
+    await expect(
+      revoke(revocationEndpoint, clientId, token, mockedRequester)
+    ).resolves.toBeUndefined();
+    expect(mockedRequester).toBeCalledTimes(1);
+    expect(mockedRequester).toBeCalledWith(revocationEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({ client_id: clientId, token }),
+    });
+  });
+});
---
+++ packages/js/src/core/revoke.ts
@@ -0,0 +1,14 @@
+import { ContentType } from '../consts';
+import { Requester } from '../utils/requester';
+
+export const revoke = async (
+  revocationEndpoint: string,
+  clientId: string,
+  token: string,
+  requester: Requester
+): Promise<void> =>
+  requester<void>(revocationEndpoint, {
+    method: 'POST',
+    headers: ContentType.formUrlEncoded,
+    body: new URLSearchParams({ client_id: clientId, token }),
+  });
