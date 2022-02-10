48bbaea52aa58b8ffd92b4a2dffda253b61a4e4d
IceHe.xyz <icehe@silverhand.io>
2022-02-10 20:03:33 +0800

    test(browser): signIn

--- packages/browser/jest.setup.js
+++ packages/browser/jest.setup.js
@@ -2,7 +2,15 @@
 // https://github.com/jsdom/jsdom/issues/1612
 /* eslint-disable @silverhand/fp/no-mutation */
 /* eslint-disable unicorn/prefer-module */
+const crypto = require('crypto');
+
+const { location } = require('jest-location-mock');
 const { TextDecoder, TextEncoder } = require('text-encoder');
 
+global.crypto = {
+  getRandomValues: (buffer) => crypto.randomFillSync(buffer),
+  subtle: crypto.webcrypto.subtle,
+};
+global.location = location;
 global.TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
