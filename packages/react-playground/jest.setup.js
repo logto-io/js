142afba193f0e9232ef368132c3eae9f0b13d681
IceHe.xyz <icehe@silverhand.io>
2022-02-14 23:11:07 +0800

    chore: improve the lint comments in all jest.setup.js (#177)

--- packages/react-playground/jest.setup.js
+++ packages/react-playground/jest.setup.js
@@ -1,12 +1,14 @@
 // Need to disable following rules to mock text-decode/text-encoder and crypto for jsdom
 // https://github.com/jsdom/jsdom/issues/1612
-/* eslint-disable @silverhand/fp/no-mutation */
 /* eslint-disable unicorn/prefer-module */
 const { Crypto } = require('@peculiar/webcrypto');
 const fetch = require('node-fetch');
 const { TextDecoder, TextEncoder } = require('text-encoder');
+/* eslint-enable unicorn/prefer-module */
 
+/* eslint-disable @silverhand/fp/no-mutation */
 global.crypto = new Crypto();
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.fetch = fetch;
+/* eslint-enable @silverhand/fp/no-mutation */
