c1a552a212fcdf5775eb3ef35757e67baef8db59
IceHe.xyz <icehe@silverhand.io>
2022-02-14 20:23:57 +0800

    chore(js,browser): improve lint comments in jest.setup.js

--- packages/browser/jest.setup.js
+++ packages/browser/jest.setup.js
@@ -1,12 +1,13 @@
 // Need to disable following rules to mock text-decode/text-encoder and crypto for jsdom
 // https://github.com/jsdom/jsdom/issues/1612
-/* eslint-disable @silverhand/fp/no-mutation */
 /* eslint-disable unicorn/prefer-module */
 const crypto = require('crypto');
 
 const { location } = require('jest-location-mock');
 const { TextDecoder, TextEncoder } = require('text-encoder');
+/* eslint-enable unicorn/prefer-module */
 
+/* eslint-disable @silverhand/fp/no-mutation */
 global.crypto = {
   getRandomValues: (buffer) => crypto.randomFillSync(buffer),
   subtle: crypto.webcrypto.subtle,
@@ -14,3 +15,4 @@ global.crypto = {
 global.location = location;
 global.TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
+/* eslint-enable @silverhand/fp/no-mutation */
