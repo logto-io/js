10076e15e6c491c2584cb8a0269f0d7bfddef526
IceHe.xyz <icehe@silverhand.io>
2022-02-22 15:30:31 +0800

    feat(browser-sample): init package

---
+++ packages/browser-sample/src/index.js
@@ -0,0 +1,5 @@
+import { init } from './app.js';
+
+const app = document.querySelector('#app');
+
+init(app);
--- packages/playground/src/consts/url.ts
+++ packages/playground/src/consts/url.ts
@@ -1,2 +1,2 @@
 export const baseUrl = `http://${String(process.env.HOST)}:${String(process.env.PORT)}`;
-export const oidcDomain = process.env.RAZZLE_OIDC_BASE_DOMAIN ?? 'N/A';
+export const oidcDomain = String(process.env.RAZZLE_OIDC_BASE_DOMAIN ?? 'N/A');
