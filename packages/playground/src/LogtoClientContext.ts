a62f17eba720f8fd209c85b04012c9115bd0fa09
IceHe.xyz <icehe@silverhand.io>
2022-02-23 17:17:56 +0800

    refactor: remove useless packages

--- packages/playground/src/LogtoClientContext.ts
+++
@@ -1,7 +0,0 @@
-import LogtoClient from '@logto/client';
-import { Nullable } from '@silverhand/essentials';
-import React from 'react';
-
-const LogtoClientContext = React.createContext<Nullable<LogtoClient>>(null);
-
-export default LogtoClientContext;
--- packages/react/src/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from '@logto/client';
-export * from './context';
-export * from './provider';
-export { default as ProtectedRoute } from './ProtectedRoute';
-export { default as useLogto } from './use-logto';
