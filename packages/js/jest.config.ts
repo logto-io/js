c7647ec7fec02565237429c477bb00befdda3eb0
IceHe.xyz <icehe@silverhand.io>
2022-02-25 14:31:47 +0800

    refactor(js): convert jest.config.js to jest.config.ts

---
+++ packages/js/jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageReporters: ['lcov', 'text-summary'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js', 'jest-matcher-specific-error'],
+};
+
+export default config;
