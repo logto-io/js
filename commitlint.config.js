6ae2e418b9383d7d9b6d343eda6ba8e03470df62
Gao Sun <gao@silverhand.io>
2022-03-10 17:10:05 +0800

    fix: publish workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,9 @@
+const { rules } = require('@commitlint/config-conventional');
+
 /** @type {import('@commitlint/types').UserConfig} **/
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [...rules['type-enum'][2], 'release']],
+  },
 };
