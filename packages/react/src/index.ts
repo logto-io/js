9e20ca5a80d9387ab7b237333e4846d84e9e8647
Charles Zhao <charleszhao@silverhand.io>
2022-03-11 18:48:46 +0800

    chore: export LogtoConfig type and bump typescript

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
 export type { LogtoContextProps } from './context';
-export type { IdTokenClaims, UserInfoResponse } from '@logto/browser';
+export type { LogtoConfig, IdTokenClaims, UserInfoResponse } from '@logto/browser';
 export * from './provider';
 export { default as useLogto } from './hooks';
