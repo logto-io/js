85fc98346ce336ebd58ba61e0cbd3d127b814a13
Charles Zhao <charleszhao@silverhand.io>
2022-05-27 21:19:06 +0800

    fix: 'LogtoClientError' should be exported as class instead of type

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,10 +1,14 @@
 export type { LogtoContextProps } from './context';
+
 export type {
   LogtoConfig,
   IdTokenClaims,
   UserInfoResponse,
-  LogtoClientError,
   LogtoClientErrorCode,
 } from '@logto/browser';
+
+export { LogtoClientError } from '@logto/browser';
+
 export * from './provider';
+
 export { useLogto, useHandleSignInCallback } from './hooks';
