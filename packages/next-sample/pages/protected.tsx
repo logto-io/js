5220edb761fe8a6e489690c46f2a3f8bb41c4b9b
wangsijie <wangsijie@silverhand.io>
2022-07-20 13:15:22 +0800

    feat(next-sample): implement nextjs sample

---
+++ packages/next-sample/pages/protected.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+// eslint-disable-next-line import/no-named-as-default
+import Router from 'next/router';
+import { useEffect } from 'react';
+import useSWR from 'swr';
+
+const Protected = () => {
+  const { data, error } = useSWR<{ data: string }, Error>('/api/protected-resource');
+
+  useEffect(() => {
+    if (error?.message === 'Unauthorized') {
+      // eslint-disable-next-line @silverhand/fp/no-mutating-methods
+      void Router.push('/api/sign-in');
+    }
+  }, [error]);
+
+  return (
+    <div>
+      <header>
+        <h1>Hello Logto.</h1>
+      </header>
+      <nav>
+        <Link href="/">
+          <a>Go Home</a>
+        </Link>
+      </nav>
+      {data && (
+        <div>
+          <h2>Protected resource:</h2>
+          <div>{data.data}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Protected;
