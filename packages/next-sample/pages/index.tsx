406082d2cc40c70a3237d66f29888e82f04ea361
Wang Sijie <wangsijie@silverhand.io>
2022-07-21 11:39:38 +0800

    feat(next-sample): implement nextjs sample (#362)

--- packages/next-sample/pages/index.tsx
+++ packages/next-sample/pages/index.tsx
@@ -1,28 +1,20 @@
 import { LogtoUser } from '@logto/next';
 import Link from 'next/link';
+import { useMemo } from 'react';
 import useSWR from 'swr';
 
 const Home = () => {
   const { data } = useSWR<LogtoUser>('/api/user');
   const { data: protectedResource } = useSWR<{ data: string }>('/api/protected-resource');
 
-  if (!data) {
-    return null;
-  }
+  const userInfo = useMemo(() => {
+    if (!data?.isAuthenticated || !data.claims) {
+      return null;
+    }
 
-  return (
-    <div>
-      Hello Logto.{' '}
-      {data.isAuthenticated ? (
-        <Link href="/api/sign-out">
-          <a>Sign Out</a>
-        </Link>
-      ) : (
-        <Link href="/api/sign-in">
-          <a>Sign In</a>
-        </Link>
-      )}
-      {data.isAuthenticated && data.claims && (
+    return (
+      <div>
+        <h2>User info:</h2>
         <table>
           <thead>
             <tr>
@@ -39,8 +31,36 @@ const Home = () => {
             ))}
           </tbody>
         </table>
+      </div>
+    );
+  }, [data]);
+
+  return (
+    <div>
+      <header>
+        <h1>Hello Logto.</h1>
+      </header>
+      <nav>
+        {data?.isAuthenticated ? (
+          <Link href="/api/sign-out">
+            <a>Sign Out</a>
+          </Link>
+        ) : (
+          <Link href="/api/sign-in">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </nav>
+      {userInfo}
+      {protectedResource && (
+        <div>
+          <h2>Protected resource:</h2>
+          <div>{protectedResource.data}</div>
+          <h3>
+            <Link href="/protected">Example: Require sign in and auto redirect</Link>
+          </h3>
+        </div>
       )}
-      <div>{protectedResource?.data}</div>
     </div>
   );
 };
