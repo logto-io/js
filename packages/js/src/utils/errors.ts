c15fd3677ed36f142618bf934f22459eb810b16b
IceHe.Life <icehe@silverhand.io>
2022-07-27 20:17:52 +0800

    chore(js): remove unused error code (#369)

--- packages/js/src/utils/errors.ts
+++ packages/js/src/utils/errors.ts
@@ -13,9 +13,6 @@ const logtoErrorCodes = Object.freeze({
     state_mismatched: 'State mismatched in the callback URI',
     missing_code: 'Missing code in the callback URI',
   },
-  requester: {
-    not_provide_fetch: 'Should provide a fetch function under Node.js',
-  },
   crypto_subtle_unavailable: 'Crypto.subtle is unavailable in insecure contexts (non-HTTPS).',
 });
 
