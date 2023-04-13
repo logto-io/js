c526d71031451b0feafe0ee3ef799dcee7bc2288
Gao Sun <gao@silverhand.io>
2023-04-13 15:55:25 +0800

    chore: upgrade essentials (#471)

--- packages/browser/jest.config.ts
+++ packages/browser/jest.config.ts
@@ -1,11 +1,17 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   collectCoverageFrom: ['src/**/*.ts'],
   coverageReporters: ['lcov', 'text-summary'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js', 'jest-matcher-specific-error'],
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['node_modules/(?!(.*(nanoid|jose|ky|@logto|@silverhand))/)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 };
 
 export default config;
--- packages/remix/jest.config.ts
+++ packages/remix/jest.config.ts
@@ -1,16 +1,17 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   collectCoverage: Boolean(process.env.CI),
   transform: {
-    "^.+\\.(t|j)sx?$": [
-      "@swc/jest",
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
       {
         sourceMaps: true,
       },
     ],
-  }
+  },
+  transformIgnorePatterns: ['node_modules/(?!(.*(nanoid|jose|ky|@logto|@silverhand))/)'],
 };
 
 export default config;
