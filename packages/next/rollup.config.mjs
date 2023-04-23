9bb4bd2094c987becdd96c2e9fc685da30edd3c8
wangsijie <wangsijie94@gmail.com>
2023-04-23 14:53:59 +0800

    chore: fix build

--- packages/next/rollup.config.mjs
+++ packages/next/rollup.config.mjs
@@ -1,1 +1,8 @@
-export { default } from '../../rollup.config.mjs';
+import baseConfigs from '../../rollup.config.mjs';
+
+const configs = {
+  ...baseConfigs,
+  input: ['src/index.ts', 'edge/index.ts'],
+};
+
+export default configs;
