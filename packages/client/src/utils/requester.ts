2acf970f8c2edbfe63beffe870478af3d1c55d33
Gao Sun <gao@silverhand.io>
2023-08-03 21:38:03 +0800

    refactor: add comments

--- packages/client/src/utils/requester.ts
+++ packages/client/src/utils/requester.ts
@@ -1,6 +1,13 @@
 import type { Requester } from '@logto/js';
 import { LogtoError, LogtoRequestError, isLogtoRequestError } from '@logto/js';
 
+/**
+ * A factory function that creates a requester by accepting a `fetch`-like function.
+ *
+ * @param fetchFunction A `fetch`-like function.
+ * @returns A requester function.
+ * @see {@link Requester}
+ */
 export const createRequester = (fetchFunction: typeof fetch): Requester => {
   return async <T>(...args: Parameters<typeof fetch>): Promise<T> => {
     const response = await fetchFunction(...args);
--- packages/js/src/types/index.ts
+++ packages/js/src/types/index.ts
@@ -3,7 +3,18 @@ export type LogtoRequestErrorBody = {
   message: string;
 };
 
+/**
+ * A request function that accepts a `fetch`-like function parameters and returns
+ * a promise with the parsed response body.
+ */
 export type Requester = <T>(...args: Parameters<typeof fetch>) => Promise<T>;
 
 // Need to align with the OIDC extraParams settings in core
+/**
+ * The interaction mode to be used for the authorization request. Note it's not
+ * a part of the OIDC standard, but a Logto-specific extension.
+ *
+ * - `signIn`: The authorization request will be initiated with a sign-in page.
+ * - `signUp`: The authorization request will be initiated with a sign-up page.
+ */
 export type InteractionMode = 'signIn' | 'signUp';
