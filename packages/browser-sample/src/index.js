43861c0ed9e96cde117eb721b723f904fa3af511
simeng-li <simeng@silverhand.io>
2024-02-20 18:28:55 +0800

    feat: upate the browser-sample
    update the browser-sample

--- packages/browser-sample/src/index.js
+++ packages/browser-sample/src/index.js
@@ -1,20 +1,36 @@
 import LogtoClient, { UserScope } from '@logto/browser';
 
-import { endpoint, appId } from './consts';
+import { endpoint, appId, resourceIndicators, resourceScopes } from './consts';
 import Callback from './pages/Callback';
 import Home from './pages/Home';
 
 import './index.scss';
 
-const logtoClient = new LogtoClient({
-  endpoint,
-  appId,
-  scopes: [UserScope.Email, UserScope.Phone, UserScope.CustomData, UserScope.Identities],
-});
-const app = document.querySelector('#app');
+(() => {
+  const logtoClient = new LogtoClient({
+    endpoint,
+    appId,
+    resources: resourceIndicators,
+    scopes: [
+      UserScope.Email,
+      UserScope.Phone,
+      UserScope.CustomData,
+      UserScope.Identities,
+      UserScope.Organizations,
+      ...resourceScopes,
+    ],
+  });
 
-// Could replace this with a formal router solution
-const isCallback = window.location.pathname.startsWith('/callback');
-const render = isCallback ? Callback : Home;
+  const main = () => {
+    const app = document.querySelector('#app');
 
-render(app, logtoClient);
+    // Could replace this with a formal router solution
+    const isCallback = window.location.pathname.startsWith('/callback');
+
+    const render = isCallback ? Callback : Home;
+
+    render(app, logtoClient);
+  };
+
+  document.addEventListener('DOMContentLoaded', main);
+})();
--- packages/browser-sample/src/pages/Home.js
+++ packages/browser-sample/src/pages/Home.js
@@ -1,40 +1,5 @@
 /* eslint-disable @silverhand/fp/no-mutation */
-import { baseUrl, redirectUrl } from '../consts';
-
-const Home = async (app, logtoClient) => {
-  if (!logtoClient) {
-    throw new Error('no logto client found');
-  }
-
-  // eslint-disable-next-line @silverhand/fp/no-let
-  let isAuthenticated = false;
-  const onClickSignIn = () => logtoClient.signIn(redirectUrl);
-  const onClickSignOut = () => logtoClient.signOut(baseUrl);
-
-  (async () => {
-    isAuthenticated = await logtoClient.isAuthenticated();
-    renderButton(container, { isAuthenticated, onClickSignIn, onClickSignOut });
-
-    if (isAuthenticated) {
-      const userInfo = await logtoClient.fetchUserInfo();
-      renderTable(container, { userInfo });
-    }
-  })();
-
-  const fragment = document.createDocumentFragment();
-
-  const container = document.createElement('div');
-  container.classList.add('container');
-
-  const h3 = document.createElement('h3');
-  h3.innerHTML = 'Logto Browser Sample';
-
-  container.append(h3);
-  renderButton(container, { isAuthenticated, onClickSignIn, onClickSignOut });
-
-  fragment.append(container);
-  app.append(fragment);
-};
+import { baseUrl, redirectUrl, resourceIndicators } from '../consts';
 
 const renderButton = (container, states) => {
   const { isAuthenticated, onClickSignIn, onClickSignOut } = states;
@@ -67,6 +32,7 @@ const renderTable = (container, states) => {
   table.append(thead);
 
   const tbody = document.createElement('tbody');
+
   for (const [key, value] of Object.entries(userInfo)) {
     const tr = document.createElement('tr');
     const tdName = document.createElement('td');
@@ -79,6 +45,101 @@ const renderTable = (container, states) => {
 
   table.append(tbody);
   container.append(table);
+};
+
+const renderTokenTable = (container, logtoClient, userInfo) => {
+  const table = document.createElement('table');
+
+  const thead = document.createElement('thead');
+  const tr = document.createElement('tr');
+  const thName = document.createElement('th');
+  const thValue = document.createElement('th');
+  const thAction = document.createElement('th');
+
+  thName.innerHTML = 'TokenType';
+  thValue.innerHTML = 'Value';
+  thAction.innerHTML = 'GetToken';
+
+  tr.append(thName, thValue, thAction);
+  thead.append(tr);
+  table.append(thead);
+
+  const tbody = document.createElement('tbody');
+
+  const renderTokenRow = (name, value, action) => {
+    const tr = document.createElement('tr');
+    const tdName = document.createElement('td');
+    const tdValue = document.createElement('td');
+    const tdAction = document.createElement('td');
+
+    tdName.innerHTML = name;
+    tdValue.innerHTML = value;
+
+    const getTokenButton = document.createElement('button');
+    getTokenButton.innerHTML = 'Get Token';
+    tdAction.append(getTokenButton);
+
+    getTokenButton.addEventListener('click', async () => {
+      const claims = await action();
+      tdValue.innerHTML = JSON.stringify(claims);
+    });
+
+    tr.append(tdName, tdValue, tdAction);
+    tbody.append(tr);
+  };
+
+  // Get resource access token
+  for (const resource of resourceIndicators) {
+    renderTokenRow(resource, '', () => logtoClient.getAccessTokenClaims(resource));
+  }
+
+  // Get Organization access token
+  const { organizations } = userInfo;
+  if (organizations) {
+    for (const organization of organizations) {
+      renderTokenRow(organization, '', () => logtoClient.getOrganizationTokenClaims(organization));
+    }
+  }
+
+  table.append(tbody);
+  container.append(table);
+};
+
+const render = async (container, logtoClient) => {
+  const onClickSignIn = () => logtoClient.signIn(redirectUrl);
+  const onClickSignOut = () => logtoClient.signOut(baseUrl);
+
+  const isAuthenticated = await logtoClient.isAuthenticated();
+
+  renderButton(container, { isAuthenticated, onClickSignIn, onClickSignOut });
+
+  if (isAuthenticated) {
+    const userInfo = await logtoClient.fetchUserInfo();
+    renderTable(container, { userInfo });
+    renderTokenTable(container, logtoClient, userInfo);
+  }
+};
+
+const Home = (app, logtoClient) => {
+  if (!logtoClient) {
+    throw new Error('no logto client found');
+  }
+
+  const fragment = document.createDocumentFragment();
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+
+  const h3 = document.createElement('h3');
+  h3.innerHTML = 'Logto Browser Sample';
+  container.append(h3);
+
+  fragment.append(container);
+  app.append(fragment);
+
+  (async () => {
+    await render(container, logtoClient);
+  })();
 };
 
 export default Home;
