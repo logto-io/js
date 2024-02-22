b2228ebc69ea1c6594decdb1f9237427c4f0013a
Gao Sun <gao@silverhand.io>
2024-02-22 14:19:41 +0800

    chore: update nuxt content

--- packages/nuxt/README.md
+++ packages/nuxt/README.md
@@ -37,6 +37,15 @@ Check out the full JS repo and try it with pnpm.
 pnpm i && pnpm dev
 ```
 
+The minimal configuration to run the playground is (use `.env` file for example):
+
+```env
+NUXT_LOGTO_ENDPOINT=<your-logto-endpoint>
+NUXT_LOGTO_APP_ID=<your-logto-app-id>
+NUXT_LOGTO_APP_SECRET=<your-logto-app-secret>
+NUXT_LOGTO_COOKIE_ENCRYPTION_KEY=<random-string>
+```
+
 ## Resources
 
 [![Website](https://img.shields.io/badge/website-logto.io-8262F8.svg)](https://logto.io/)
--- packages/nuxt/src/runtime/composables/use-logto-user.ts
+++ packages/nuxt/src/runtime/composables/use-logto-user.ts
@@ -4,6 +4,24 @@ import { LogtoStateKey } from '../utils/constants';
 
 import { shallowRef as shallowReference, useNuxtApp, useState } from '#imports';
 
+/**
+ * Get the Logto user information. If the user is not signed in, this composable will return
+ * `undefined`.
+ *
+ * Note: This composable relies on the SSR context which is filled by the Logto event handler. Once
+ * the user is signed in, the user information will be available in both the server and client side.
+ *
+ * @returns The user information if the user is signed in, otherwise `undefined`.
+ *
+ * @example
+ * ```ts
+ * const user = useLogtoUser();
+ *
+ * if (user) {
+ *   console.log('User is signed in:', user); // { sub: '123', ... }
+ * }
+ * ```
+ */
 export default function useLogtoUser() {
   const nuxtApp = useNuxtApp();
   const user = useState<UserInfoResponse | undefined>(LogtoStateKey.User, () =>
