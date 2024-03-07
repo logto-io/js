6da8c25f52f6d7e9d3aef226309d6a861179c114
Gao Sun <gao@silverhand.io>
2024-03-07 10:44:18 +0800

    chore: us typescript config

--- commitlint.config.cjs
+++
@@ -1,9 +0,0 @@
-const { rules } = require('@commitlint/config-conventional');
-
-/** @type {import('@commitlint/types').UserConfig} **/
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', [...rules['type-enum'][2], 'release']],
-  },
-};
