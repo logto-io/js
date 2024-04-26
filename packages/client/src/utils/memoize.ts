bc3e8da9ab4007c40a1c5e1d78070309c70f09c4
wangsijie <wangsijie@silverhand.io>
2024-04-26 09:51:13 +0800

    fix(client): fix memoize cache key (#712)
    fixed #771

--- packages/client/src/utils/memoize.ts
+++ packages/client/src/utils/memoize.ts
@@ -2,7 +2,7 @@ export function memoize<Args extends unknown[], Return>(run: (...args: Args) => 
   const promiseCache = new Map<unknown, Promise<Return>>();
 
   const memoized = async function (this: unknown, ...args: Args): Promise<Return> {
-    const promiseKey = args[0];
+    const promiseKey = JSON.stringify(args);
     const cachedPromise = promiseCache.get(promiseKey);
 
     if (cachedPromise) {
