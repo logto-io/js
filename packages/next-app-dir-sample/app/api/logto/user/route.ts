64a3ae58cf6214bdccf4d80626719b288984eaa9
wangsijie <wangsijie@silverhand.io>
2024-10-09 14:47:56 +0800

    chore: remove next-app-dir-sample (#798)

--- packages/next-app-dir-sample/app/api/logto/user/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest } from 'next/server';
-
-import { logtoClient } from '../../../../libraries/logto-edge';
-
-export const runtime = 'edge';
-
-export async function GET(request: NextRequest) {
-  return logtoClient.handleUser()(request);
-}
