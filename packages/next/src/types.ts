a0f91a3acf84c34eac6fa5b89348896a8ea2bfe4
wangsijie <wangsijie@silverhand.io>
2025-01-07 19:40:57 +0800

    feat(next): external storage (#896)

--- packages/next/src/types.ts
+++ packages/next/src/types.ts
@@ -1,11 +1,18 @@
-import type { LogtoConfig } from '@logto/node';
+import type { LogtoConfig, SessionWrapper } from '@logto/node';
 import type NodeClient from '@logto/node';
 import { type NextApiRequest, type NextApiResponse } from 'next';
 
 export type LogtoNextConfig = LogtoConfig & {
-  cookieSecret: string;
   cookieSecure: boolean;
   baseUrl: string;
+  /**
+   * Can be provided to use custom session wrapper,
+   * for example, to use external storage solutions,
+   * you can save the session data in external storage and return a key in the sessionWrapper.wrap method,
+   * then use the key to get the session data from external storage in the sessionWrapper.unwrap method.
+   */
+  sessionWrapper?: SessionWrapper;
+  cookieSecret?: string;
 };
 
 export type Adapters = {
