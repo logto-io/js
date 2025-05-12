dd8993903a2fa1861409854f1629f61d73daea56
wangsijie <wangsijie@silverhand.io>
2025-05-12 09:53:39 +0800

    feat: add remix sample project (#952)

---
+++ packages/remix-sample/app/services/auth.server.ts
@@ -0,0 +1,25 @@
+import { makeLogtoRemix } from '@logto/remix';
+import { createCookieSessionStorage } from '@remix-run/node';
+
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: 'logto-session',
+    maxAge: 14 * 24 * 60 * 60,
+    // Remember to change this secret in production
+    secrets: [process.env.SESSION_SECRET ?? 'secr3tSession'],
+  },
+});
+
+if (!process.env.LOGTO_ENDPOINT || !process.env.LOGTO_APP_ID || !process.env.LOGTO_APP_SECRET) {
+  throw new Error('Missing Logto environment variables');
+}
+
+export const logto = makeLogtoRemix(
+  {
+    endpoint: process.env.LOGTO_ENDPOINT,
+    appId: process.env.LOGTO_APP_ID,
+    appSecret: process.env.LOGTO_APP_SECRET,
+    baseUrl: process.env.LOGTO_BASE_URL ?? 'http://localhost:5173',
+  },
+  { sessionStorage }
+);
